f24ec800d31df7f1f903764cfb89c7ddb8726320
kazukishirasu <s19c1057fa@s.chibakoudai.jp>
2023-04-04 12:24:18 +0900

    add orientation

--- test_wp_nav/run.py
+++ test_wp_nav/run.py
@@ -9,6 +9,8 @@ import yaml
 from rclpy.action import ActionClient
 from nav2_msgs.action import NavigateToPose
 import math
+import tf_transformations
+from tf_transformations import euler_from_quaternion
 
 class test_wp_nav(Node):
     def __init__(self):
@@ -19,7 +21,7 @@ class test_wp_nav(Node):
         self.pose_x, self.pose_y = 0.0, 0.0
         self.dist_err = 0.4
         self.wp_num = 0
-        self.waypoint_file = '/home/kazuki/test_ws/src/test_wp_nav/waypoints/waypoints.yaml'
+        self.waypoint_file = '/home/kazuki/ros2_ws/src/test_wp_nav/waypoints/waypoints.yaml'
         with open(self.waypoint_file, 'r') as yml:
             self.waypoint = yaml.safe_load(yml)
 
@@ -33,7 +35,7 @@ class test_wp_nav(Node):
         self.pose_x = data.pose.pose.position.x
         self.pose_y = data.pose.pose.position.y
     
-    def send_goal(self, wp_num):
+    def send_goal(self, wp_num, orientation):
         while not self.nav_client.wait_for_server(timeout_sec=1.0):
             self.get_logger().info('Waiting for action server')
         goal_pose = NavigateToPose.Goal()
@@ -41,12 +43,20 @@ class test_wp_nav(Node):
         goal_pose.pose.pose.position.x = self.waypoint['waypoints'][wp_num]['point']['x']
         goal_pose.pose.pose.position.y = self.waypoint['waypoints'][wp_num]['point']['y']
         goal_pose.pose.pose.position.z = self.waypoint['waypoints'][wp_num]['point']['z']
-        goal_pose.pose.pose.orientation.x = 0.0
-        goal_pose.pose.pose.orientation.y = 0.0
-        goal_pose.pose.pose.orientation.z = 0.0
-        goal_pose.pose.pose.orientation.w = 0.1
+        goal_pose.pose.pose.orientation.x = orientation[0]
+        goal_pose.pose.pose.orientation.y = orientation[1]
+        goal_pose.pose.pose.orientation.z = orientation[2]
+        goal_pose.pose.pose.orientation.w = orientation[3]
         goal = self.nav_client.send_goal_async(goal_pose)
-        return goal
+    
+    def calc_deg(self, wp_num):
+        pose_x = self.pose_x
+        pose_y = self.pose_y
+        goal_x = self.waypoint['waypoints'][wp_num]['point']['x']
+        goal_y = self.waypoint['waypoints'][wp_num]['point']['y']
+        radian = math.atan2(goal_y - pose_y, goal_x - pose_x)
+        orientation = tf_transformations.quaternion_from_euler(0, 0, radian)
+        return orientation
     
     def check_distance(self, pose_x, pose_y):
         goal_x = self.waypoint['waypoints'][self.wp_num]['point']['x']
@@ -64,11 +74,13 @@ class test_wp_nav(Node):
         
     def navigation(self):
         if self.wp_num < len(self.waypoint['waypoints']):
-            self.send_goal(self.wp_num)
+            orientation = self.calc_deg(self.wp_num)
+            self.send_goal(self.wp_num, orientation)
             dist = self.check_distance(self.pose_x, self.pose_y)
             self.status(dist)
         else:
             self.get_logger().info('Finish navigation')
+            self.wp_num = 0
 
 def main(args=None):
     rclpy.init(args=args)
